ccc6facb66125626c82dbfec49a6bb2c1313b16b
DevTheofanes <devtheofanes@gmail.com>
2021-10-11 14:07:26 -0300

    fix past

--- src/pages/NewRating/index.jsx
+++ src/pages/NewRating/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 import { MdPhotoSizeSelectActual } from 'react-icons/md';
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 
@@ -20,8 +21,55 @@ import {
 import { HeaderTopBarComponent } from '../../components/HeaderTopBar';
 import { FooterComponent } from '../../components/Footer';
 
+import api from '../../services/api';
+import history from '../../services/history';
+
 
 export function NewRating() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [profile, setProfile] = useState("")
+  const [comments, setComments] = useState("")
+
+  async function getUrl(image){
+    const data = new FormData();
+    data.append('image', image);
+
+    const response = await api.post("/files/url", data)
+    
+    return response.data;
+  }
+
+  async function postRating(data){
+    try {
+      const response = await api.post('/user/3/rating', data)
+      toast.success(`${response.data.name}! Sua avaliação foi criada com sucesso.`)
+      
+      history.push("/");
+    } catch (error) {
+      toast.error(error.response.data.error)
+      console.log(error.response.data)
+    }
+  }
+
+  async function handleNewRating(){
+    if(!name || !email || !profile || !comments){
+      return toast.warn("Preencha todos os campos.")
+    }
+
+    const avatarUrl = await getUrl(profile)
+
+    const data = {
+      name, 
+      email, 
+      rating: "4",
+      avatarUrl, 
+      comments
+    }
+
+    await postRating(data)
+  }
+
   return (
     <Container>
       <HeaderTopBarComponent />
@@ -34,12 +82,12 @@ export function NewRating() {
             <FormColumn>
               <FormInput>
                 <span>Seu nome </span>
-                <input type="text" />
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
               </FormInput>
 
               <FormInput>
                 <span>Email </span>
-                <input type="text" />
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
               </FormInput>
             </FormColumn>
 
@@ -48,7 +96,7 @@ export function NewRating() {
               <label for="perfil">
                 <MdPhotoSizeSelectActual color="#999" size={32} />
               </label>
-              <input type="file" id="perfil" name="perfil" />
+              <input onChange={(e) => setProfile(e.target.files[0])} type="file" id="perfil" name="perfil" />
             </FormInputPrimary>
           </InfosUser>
 
@@ -62,11 +110,11 @@ export function NewRating() {
 
           <FormInput className="unicInput">
             <span>Messagem </span>
-            <textarea name="" id="" cols="30" rows="3"></textarea>
+            <textarea name="" id="" cols="30" rows="3" value={comments} onChange={(e) => setComments(e.target.value)}></textarea>
           </FormInput>
 
           <FormSubmit>
-            <FormSubmitButton>
+            <FormSubmitButton onClick={() => handleNewRating()}>
               Enviar avaliação
             </FormSubmitButton>
           </FormSubmit>
