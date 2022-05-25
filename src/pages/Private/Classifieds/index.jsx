a3813dadc77351de3e672eee9ea4cf46ea12be43
DevTheofanes <devtheofanes@gmail.com>
2022-05-25 15:50:04 -0300

    fix: corretion in loadClassifieds

--- src/pages/Private/Classifieds/index.jsx
+++ src/pages/Private/Classifieds/index.jsx
@@ -20,35 +20,37 @@ export function DashboardClassifieds() {
   const [classifieds, setClassifieds] = useState([]);
   const { user } = useUser();
 
-  useEffect(() => {
-    async function loadClassifieds() {
-      try {
-        const response = await api.get(`/user/${user.id}/classifieds`);
-        console.log(response.data);
-        setClassifieds(response.data);
-      } catch (error) {
-        toast.warn('Algo deu errado, tente novamente mais tarde.');
-        console.log(error.response.data);
-      }
+  async function loadClassifieds() {
+    try {
+      const response = await api.get(`/user/${user.id}/classifieds`);
+      console.log(response.data);
+      setClassifieds(response.data);
+    } catch (error) {
+      toast.warn('Algo deu errado, tente novamente mais tarde.');
+      console.log(error.response.data);
     }
+  }
 
-    loadClassifieds();
+  useEffect(() => {
+    if (user) {
+      loadClassifieds();
+    }
   }, [user]);
 
   return (
     <DashboardContainer>
       <DashboardContainerButtons>
-        <a href="/cadastro/classificado">
-          Novo Anúncio
-        </a>
+        <a href="/cadastro/classificado">Novo Anúncio</a>
       </DashboardContainerButtons>
 
       <DashboardContainerTable title="classified">
-        {
-          classifieds.map((classified) => (
-            <DashboardItemTable key={classified.id} title="classified" classified={classified} />
-          ))
-        }
+        {classifieds.map((classified) => (
+          <DashboardItemTable
+            key={classified.id}
+            title="classified"
+            classified={classified}
+          />
+        ))}
         {/* <DashboardItemTable title="classified" />
         <DashboardItemTable title="classified" />
         <DashboardItemTable title="classified" /> */}
